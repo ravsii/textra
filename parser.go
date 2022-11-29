269947cd208c3ac62d479400481bf0cf1d89272a
Alex <naplle58@gmail.com>
2022-11-30 00:16:42 +0300

    Split tag parser

--- parser.go
+++ parser.go
@@ -13,27 +13,29 @@ func parseTags(tag reflect.StructTag) Tags {
 	parsed := make(Tags, 0, len(tags))
 
 	for _, tagStr := range tags {
-		tagSplitted := strings.Split(tagStr, ":")
-
-		k := tagSplitted[0]
-
-		// Removing quotes, "value,  optional" -> value,  optional
-		v := tagSplitted[1][1 : len(tagSplitted[1])-1]
-		// Removing extra spaces, value,  optional -> value,optional
-		v = strings.ReplaceAll(v, " ", "")
-		vs := strings.Split(v, ",")
-
-		tag := Tag{
-			Tag:   k,
-			Value: vs[0],
-		}
-
-		if len(vs) > 1 {
-			tag.Optional = vs[1:]
-		}
-
-		parsed = append(parsed, tag)
+		parsed = append(parsed, parseTag(tagStr))
 	}
 
 	return parsed
+}
+
+func parseTag(tagStr string) Tag {
+	splitted := strings.Split(tagStr, ":")
+
+	// Removing quotes, "value,  optional" -> value,  optional
+	v := splitted[1][1 : len(splitted[1])-1]
+	// Removing extra spaces, value,  optional -> value,optional
+	v = strings.ReplaceAll(v, " ", "")
+	vs := strings.Split(v, ",")
+
+	tag := Tag{
+		Tag:   splitted[0],
+		Value: vs[0],
+	}
+
+	if len(vs) > 1 {
+		tag.Optional = vs[1:]
+	}
+
+	return tag
 }
