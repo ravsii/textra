78321f2c622d9d66e9c28c904ef5ed80fd33d1e1
Alex <naplle58@gmail.com>
2022-12-05 22:47:04 +0300

    Add json tags for Field and FieldTag

--- textra.go
+++ textra.go
@@ -10,18 +10,18 @@ import (
 
 // Field represents a one struct field.
 type Field struct {
-	Name string
+	Name string `json:"name"`
 	// Type is stringified type, like "time.Time" or "*string"
-	Type string
-	Tags Tags
+	Type string `json:"type"`
+	Tags Tags   `json:"tags"`
 }
 
 // FieldTag is like but it has only one tag.
 // It's used as output of some functions (like Only()).
 type FieldTag struct {
-	Name string
-	Type string
-	Tag  Tag
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Tag  Tag    `json:"tag"`
 }
 
 // Struct represents a single struct.
