b2b2acedcd642668cac3afef3947466b79e77cac
Alex <naplle58@gmail.com>
2023-04-29 00:04:32 +0300

    refactor: tests

--- struct_test.go
+++ struct_test.go
@@ -7,100 +7,169 @@ import (
 	"github.com/ravsii/textra"
 )
 
-func TestGetField(t *testing.T) {
-	type Tester struct {
+// Here's a list of test structs you can you for testing.
+type (
+	TestEmpty struct{}
+	TestOne   struct {
 		Tag struct{} `json:"tag"`
 	}
-
-	data := textra.Extract(Tester{})
-
-	if _, ok := data.Field("Tag"); !ok {
-		t.Errorf("Field: %s should be found", "Tag")
+	TestMultiple struct {
+		Tag1 struct{} `json:"tag1"`
+		Tag2 struct{} `json:"tag2" pg:"tag2" sql:"tag2, pk"`
+		Tag3 struct{} `json:"tag3" sql:"tag3, pk"`
+		Tag4 struct{} `json:"tag4" gorm:",pk" sql:"tag4, pk"`
 	}
+)
 
+func TestGetField(t *testing.T) {
+	data := textra.Extract((*TestOne)(nil))
+	if _, ok := data.Field("Tag"); !ok {
+		t.Errorf("Field() =  %t, want: found", ok)
+	}
 	if _, ok := data.Field("NotExists"); ok {
-		t.Errorf("Field: %s should NOT be found", "NotExists")
+		t.Errorf("Field() =  %t, want: not found", ok)
 	}
 }
 
 func TestByTagName(t *testing.T) {
-	type Tester struct {
-		Tag1 struct{} `json:"tag1"`
-		Tag2 struct{} `json:"tag2" sql:"tag2, pk"`
-	}
-
-	testCases := []struct {
-		name    string
+	tests := []struct {
 		tagName string
 		want    textra.Struct
 	}{
-		{"json", "json", textra.Struct{
-			textra.Field{
-				Name: "Tag1",
-				Type: "struct",
-				Tags: textra.Tags{
-					{"json", "tag1", nil},
-				},
-			},
-			textra.Field{
+		{"pg", textra.Struct{
+			{
 				Name: "Tag2",
 				Type: "struct",
 				Tags: textra.Tags{
 					{"json", "tag2", nil},
+					{"pg", "tag2", nil},
 					{"sql", "tag2", []string{"pk"}},
 				},
 			},
 		}},
-		{"sql", "sql", textra.Struct{
-			textra.Field{
+		{"sql", textra.Struct{
+			{
 				Name: "Tag2",
 				Type: "struct",
 				Tags: textra.Tags{
 					{"json", "tag2", nil},
+					{"pg", "tag2", nil},
 					{"sql", "tag2", []string{"pk"}},
 				},
 			},
+			{
+				Name: "Tag3",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag3", nil},
+					{"sql", "tag3", []string{"pk"}},
+				},
+			},
+			{
+				Name: "Tag4",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag4", nil},
+					{"gorm", "", []string{"pk"}},
+					{"sql", "tag4", []string{"pk"}},
+				},
+			},
 		}},
-		{"non-existent", "nonexistent", nil},
+		{"nonexistent", nil},
 	}
 
-	data := textra.Extract(Tester{})
+	data := textra.Extract((*TestMultiple)(nil))
 
-	for _, testCase := range testCases {
-		testCase := testCase
-
-		got := data.ByTagName(testCase.tagName)
-
-		// nil / empty checks
-		if len(got) == len(testCase.want) && reflect.DeepEqual(got, textra.Struct{}) {
-			continue
-		}
-
-		if !reflect.DeepEqual(got, testCase.want) {
-			t.Errorf("%s: got %v want %v", testCase.name, got, testCase.want)
-		}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.tagName, func(t *testing.T) {
+			t.Parallel()
+			got := data.ByTagName(tt.tagName)
+			if !checkEqual(t, got, tt.want) {
+				t.Errorf("TestByTagName() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestByAnyTagName(t *testing.T) {
-	type Tester struct {
-		Tag1 struct{} `json:"tag1"`
-		Tag2 struct{} `json:"tag2" sql:"tag2, pk"`
+func TestByTagNameAny(t *testing.T) {
+	tests := []struct {
+		name     string
+		tagNames []string
+		want     textra.Struct
+	}{
+		{"pg & gorm", []string{"pg", "gorm"}, textra.Struct{
+			{
+				Name: "Tag2",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag2", nil},
+					{"pg", "tag2", nil},
+					{"sql", "tag2", []string{"pk"}},
+				},
+			},
+			{
+				Name: "Tag4",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag4", nil},
+					{"gorm", "", []string{"pk"}},
+					{"sql", "tag4", []string{"pk"}},
+				},
+			},
+		}},
+		{"sql only", []string{"sql"}, textra.Struct{
+			{
+				Name: "Tag2",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag2", nil},
+					{"pg", "tag2", nil},
+					{"sql", "tag2", []string{"pk"}},
+				},
+			},
+			{
+				Name: "Tag3",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag3", nil},
+					{"sql", "tag3", []string{"pk"}},
+				},
+			},
+			{
+				Name: "Tag4",
+				Type: "struct",
+				Tags: textra.Tags{
+					{"json", "tag4", nil},
+					{"gorm", "", []string{"pk"}},
+					{"sql", "tag4", []string{"pk"}},
+				},
+			},
+		}},
+		{"non-existent", []string{}, nil},
 	}
 
-	testCases := []struct {
+	data := textra.Extract((*TestMultiple)(nil))
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := data.ByTagNameAny(tt.tagNames...)
+			if !checkEqual(t, got, tt.want) {
+				t.Errorf("TestByTagNameAny() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByTagNameAll(t *testing.T) {
+	tests := []struct {
 		name     string
 		tagNames []string
 		want     textra.Struct
 	}{
 		{"json & sql", []string{"json", "sql"}, textra.Struct{
-			textra.Field{
-				Name: "Tag1",
-				Type: "struct",
-				Tags: textra.Tags{
-					{"json", "tag1", nil},
-				},
-			},
 			textra.Field{
 				Name: "Tag2",
 				Type: "struct",
@@ -123,73 +192,17 @@ func TestByAnyTagName(t *testing.T) {
 		{"non-existent", []string{}, nil},
 	}
 
-	data := textra.Extract(Tester{})
+	data := textra.Extract((*TestMultiple)(nil))
 
-	for _, testCase := range testCases {
-		testCase := testCase
-
-		got := data.ByTagNameAny(testCase.tagNames...)
-
-		// nil / empty checks
-		if len(got) == len(testCase.want) && reflect.DeepEqual(got, textra.Struct{}) {
-			continue
-		}
-
-		if !reflect.DeepEqual(got, testCase.want) {
-			t.Errorf("%s: got %v want %v", testCase.name, got, testCase.want)
-		}
-	}
-}
-
-func TestByTagNames(t *testing.T) {
-	type Tester struct {
-		Tag1 struct{} `json:"tag1"`
-		Tag2 struct{} `json:"tag2" sql:"tag2, pk"`
-	}
-
-	testCases := []struct {
-		name     string
-		tagNames []string
-		want     textra.Struct
-	}{
-		{"json & sql", []string{"json", "sql"}, textra.Struct{
-			textra.Field{
-				Name: "Tag2",
-				Type: "struct",
-				Tags: textra.Tags{
-					{"json", "tag2", nil},
-					{"sql", "tag2", []string{"pk"}},
-				},
-			},
-		}},
-		{"sql only", []string{"sql"}, textra.Struct{
-			textra.Field{
-				Name: "Tag2",
-				Type: "struct",
-				Tags: textra.Tags{
-					{"json", "tag2", nil},
-					{"sql", "tag2", []string{"pk"}},
-				},
-			},
-		}},
-		{"non-existent", []string{}, nil},
-	}
-
-	data := textra.Extract(Tester{})
-
-	for _, testCase := range testCases {
-		testCase := testCase
-
-		got := data.ByTagNameAll(testCase.tagNames...)
-
-		// nil / empty checks
-		if len(got) == len(testCase.want) && reflect.DeepEqual(got, textra.Struct{}) {
-			continue
-		}
-
-		if !reflect.DeepEqual(got, testCase.want) {
-			t.Errorf("%s: got %v want %v", testCase.name, got, testCase.want)
-		}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := data.ByTagNameAll(tt.tagNames...)
+			if !checkEqual(t, got, tt.want) {
+				t.Errorf("TestByTagNameAll() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
@@ -199,7 +212,7 @@ func TestFilterFunc(t *testing.T) {
 		Tag2 struct{} `json:"tag2" sql:"tag2, pk"`
 	}
 
-	testCases := []struct {
+	tests := []struct {
 		name       string
 		filterFunc func(textra.Field) bool
 		want       textra.Struct
@@ -228,19 +241,15 @@ func TestFilterFunc(t *testing.T) {
 
 	data := textra.Extract(Tester{})
 
-	for _, testCase := range testCases {
-		testCase := testCase
-
-		got := data.FilterFunc(testCase.filterFunc)
-
-		// nil / empty checks
-		if len(got) == len(testCase.want) && reflect.DeepEqual(got, textra.Struct{}) {
-			continue
-		}
-
-		if !reflect.DeepEqual(got, testCase.want) {
-			t.Errorf("%s: got %v want %v", testCase.name, got, testCase.want)
-		}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := data.FilterFunc(tt.filterFunc)
+			if !checkEqual(t, got, tt.want) {
+				t.Errorf("TestFilterFunc() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
@@ -254,7 +263,7 @@ func TestRemoveEmpty(t *testing.T) {
 		Tag1  struct{} `json:"tag1"`
 	}
 
-	testCases := []struct {
+	tests := []struct {
 		name string
 		str  interface{}
 		want textra.Struct
@@ -280,18 +289,130 @@ func TestRemoveEmpty(t *testing.T) {
 		{"non-existent", struct{}{}, nil},
 	}
 
-	for _, testCase := range testCases {
-		testCase := testCase
-
-		got := textra.Extract(testCase.str).RemoveEmpty()
-
-		// nil / empty checks
-		if len(got) == len(testCase.want) && reflect.DeepEqual(got, textra.Struct{}) {
-			continue
-		}
-
-		if !reflect.DeepEqual(got, testCase.want) {
-			t.Errorf("%s: got %v want %v", testCase.name, got, testCase.want)
-		}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := textra.Extract(tt.str).RemoveEmpty()
+			if !checkEqual(t, got, tt.want) {
+				t.Errorf("TestRemoveEmpty() = %v, want %v", got, tt.want)
+			}
+		})
 	}
+}
+
+func TestOnlyTag(t *testing.T) {
+	type empty struct{}
+	type onlyOne struct {
+		Example string `json:"ex1"`
+	}
+	type many struct {
+		Str1 string `json:"str1" pg:"str1" sql:"str1"`
+		Str2 string `json:"str2" pg:"str2"`
+	}
+
+	tests := []struct {
+		name string
+		str  interface{}
+		only string
+		want []textra.FieldTag
+	}{
+		{"empty", (*empty)(nil), "test", []textra.FieldTag{}},
+		{"only one", (*onlyOne)(nil), "json",
+			[]textra.FieldTag{
+				{
+					Name: "Example",
+					Type: "string",
+					Tag:  textra.Tag{Tag: "json", Value: "ex1"},
+				},
+			},
+		},
+		{"many json", (*many)(nil), "json",
+			[]textra.FieldTag{
+				{
+					Name: "Str1",
+					Type: "string",
+					Tag:  textra.Tag{Tag: "json", Value: "str1"},
+				},
+				{
+					Name: "Str2",
+					Type: "string",
+					Tag:  textra.Tag{Tag: "json", Value: "str2"},
+				},
+			},
+		},
+		{"many sql", (*many)(nil), "pg",
+			[]textra.FieldTag{
+				{
+					Name: "Str1",
+					Type: "string",
+					Tag:  textra.Tag{Tag: "pg", Value: "str1"},
+				},
+				{
+					Name: "Str2",
+					Type: "string",
+					Tag:  textra.Tag{Tag: "pg", Value: "str2"},
+				},
+			},
+		},
+		{"many pg", (*many)(nil), "sql",
+			[]textra.FieldTag{
+				{
+					Name: "Str1",
+					Type: "string",
+					Tag:  textra.Tag{Tag: "sql", Value: "str1"},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			extracted := textra.Extract(tt.str)
+			got := extracted.OnlyTag(tt.only)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OnlyTag() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestString(t *testing.T) {
+	tests := []struct {
+		name string
+		str  interface{}
+		want string
+	}{
+		{"TestEmpty", (*TestEmpty)(nil), ``},
+		{"TestOne", (*TestOne)(nil), `Tag(struct):[json:"tag"]`},
+		{
+			"TestMultiple",
+			(*TestMultiple)(nil),
+			`Tag1(struct):[json:"tag1"]Tag2(struct):[json:"tag2" pg:"tag2" sql:"tag2,pk"]Tag3(struct):[json:"tag3" sql:"tag3,pk"]Tag4(struct):[json:"tag4" gorm:",pk" sql:"tag4,pk"]`,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if got := textra.Extract(tt.str).String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func checkEqual(t *testing.T, got, want textra.Struct) bool {
+	// nil / empty checks
+	if len(got) == len(want) && reflect.DeepEqual(got, textra.Struct{}) {
+		return true
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		return false
+	}
+
+	return true
 }
