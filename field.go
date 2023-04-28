9c546c42de18101f7e9ea93b63b49238d15e3492
Alex <naplle58@gmail.com>
2023-04-28 23:36:39 +0300

    Add "omitempty" to field tags

--- field.go
+++ field.go
@@ -9,7 +9,7 @@ type Field struct {
 	Name string `json:"name"`
 	// Type is a type of a field, like "time.Time" or "*string"
 	Type string `json:"type"`
-	Tags Tags   `json:"tags"`
+	Tags Tags   `json:"tags,omitempty"`
 }
 
 // FieldTag is like Field but it has only one tag.
@@ -18,7 +18,7 @@ type FieldTag struct {
 	Name string `json:"name"`
 	// Type is a type of a field, like "time.Time" or "*string"
 	Type string `json:"type"`
-	Tag  Tag    `json:"tag"`
+	Tag  Tag    `json:"tag,omitempty"`
 }
 
 func (f Field) String() string {
